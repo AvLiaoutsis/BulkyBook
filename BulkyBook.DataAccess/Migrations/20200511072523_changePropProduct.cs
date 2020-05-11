e0948caa17750d3b79de6b8c057dadbd1ef01bb5
Αβραάμ Λιαούτσης <av.liaoutsis@outlook.com>
2020-05-11 11:45:30 +0300

    Product CRUD

---
+++ BulkyBook.DataAccess/Migrations/20200511072523_changePropProduct.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class changePropProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dictionary",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dictionary",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
